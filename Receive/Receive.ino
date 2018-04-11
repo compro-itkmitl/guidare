18f2e985a545a699e869bb9c802f59a0b5f4d0ec
boomNDS <boomsixboom@gmail.com>
2018-04-11 11:50:55 +0700

    add motor con

--- Receive/Receive.ino
+++ Receive/Receive.ino
@@ -4,6 +4,15 @@ RF24 radio(9, 10);//commu NRF24L01
 const uint64_t send_pipe=0xB01DFACECEL;
 const uint64_t recv_pipe=0xDEADBEEFF1L;
 #define CODE_NONE 0
+#define forward 1
+#define backward 2
+#define left 3
+#define right 4
+#define stopi 5
+#define motor_aa 3
+#define motor_ab 4
+#define motor_ba 6
+#define motor_bb 7
 
 void setup()
 {
@@ -32,7 +41,39 @@ void loop()
     radio.write(&motor_code, sizeof(unsigned long));
     radio.startListening();
     Serial.println(motor_code);
- 
+if(motor_code==backward){
+      Serial.println("backward");
+      digitalWrite(motor_aa,LOW);
+      digitalWrite(motor_ab,HIGH);
+      digitalWrite(motor_ba,LOW);
+      digitalWrite(motor_bb,HIGH);
+    }else if(motor_code==forward){
+      Serial.println("forward");
+      digitalWrite(motor_aa,HIGH);
+      digitalWrite(motor_ab,LOW);
+      digitalWrite(motor_ba,HIGH);
+      digitalWrite(motor_bb,LOW);
+    }else if(motor_code==left){
+      Serial.println("left");
+      digitalWrite(motor_aa,HIGH);
+      digitalWrite(motor_ab,LOW);
+      digitalWrite(motor_ba,LOW);
+      digitalWrite(motor_bb,HIGH);
+    }else if(motor_code==right){
+      Serial.println("right");
+      digitalWrite(motor_aa,LOW);
+      digitalWrite(motor_ab,HIGH);
+      digitalWrite(motor_ba,HIGH);
+      digitalWrite(motor_bb,LOW); 
+    }else if(motor_code==stopi){
+      Serial.println("stop");
+      digitalWrite(motor_aa,HIGH);
+      digitalWrite(motor_ab,HIGH);
+      digitalWrite(motor_ba,HIGH);
+      digitalWrite(motor_bb,HIGH);
+    }else{
+      Serial.println("error..");
+    }
   }
 }
 
